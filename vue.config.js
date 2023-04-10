c2c53bf5b206cc8279cad1a00d745951e4589ffa
meq1985 <meq1985@hotmail.com>
2023-04-10 12:03:29 -0300

    configureWebpack

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,15 @@ module.exports = defineConfig({
   
 })
 
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js')
+      }
+    }
+  }
+}
 
